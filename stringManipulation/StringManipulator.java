70062e9f27359752bff179c35bb0de2e9faf663d
Motuma Ayana <motuma.ayana1@gmail.com>
2017-10-30 13:36:10 -0400

    adding conditions

---
+++ stringManipulation/StringManipulator.java
@@ -0,0 +1,6 @@
+public class StringManpulator{
+    public String trimAndConcat(String str1 , String str){
+        String ninja = String.format("%s%s", str1, str1);
+        return ninja;
+    }
+}
---
+++ stringManipulation/StringManipulatorTest.java
@@ -0,0 +1,7 @@
+class StringManipulatorTest {
+    public static void main(String[] args) {
+        StringManipulator manipulator = new StringManipulator();
+        String str = manipulator.trimAndConcat("    Hello     ","     World    ");
+        System.out.println(str); // HelloWorld 
+    }
+}
